a87ca46e8966b4077a066bab1a092a13c2df9349
Md. Abu Hena Mostofa <251-15-127@diu.edu.bd>
2026-08-10 00:48:35 +0600

    Add ParallelLetterFrequency class for letter counting
    Implemented a class to count letter frequencies in parallel using multiple threads.

--- Week13/Problem03.java
+++ Week13/Problem03.java
@@ -8,6 +8,72 @@ package com.mycompany.week13;
  *
  * @author User
  */
-public class Problem03 {
-    
+import java.util.Map;
+import java.util.HashMap;
+import java.util.concurrent.*;
+
+class ParallelLetterFrequency {
+
+    private String[] texts;
+
+    ParallelLetterFrequency(String[] texts) {
+        this.texts = texts;
+    }
+
+    Map<Character, Integer> countLetters() {
+
+        Map<Character, Integer> result = new HashMap<>();
+
+        ExecutorService executor =
+                Executors.newFixedThreadPool(
+                        Runtime.getRuntime().availableProcessors()
+                );
+
+        try {
+            Future<Map<Character, Integer>>[] futures =
+                    new Future[texts.length];
+
+            for (int i = 0; i < texts.length; i++) {
+                final String text = texts[i];
+
+                futures[i] = executor.submit(() -> {
+                    Map<Character, Integer> localMap = new HashMap<>();
+
+                    for (char c : text.toLowerCase().toCharArray()) {
+                        if (Character.isLetter(c)) {
+                            localMap.put(
+                                c,
+                                localMap.getOrDefault(c, 0) + 1
+                            );
+                        }
+                    }
+
+                    return localMap;
+                });
+            }
+
+            for (Future<Map<Character, Integer>> future : futures) {
+                Map<Character, Integer> localMap = future.get();
+
+                for (Map.Entry<Character, Integer> entry
+                        : localMap.entrySet()) {
+
+                    result.merge(
+                        entry.getKey(),
+                        entry.getValue(),
+                        Integer::sum
+                    );
+                }
+            }
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executor.shutdown();
+        }
+
+        return result;
+    }
 }

f064623c6d189fd1c0990225daefdbd6838cf325
Md. Abu Hena Mostofa <251-15-127@diu.edu.bd>
2026-08-10 00:51:36 +0600

    Add server functionality for arithmetic operations
    Implement a simple server that performs basic arithmetic operations based on client input.

--- Week13/Problem04.java
+++ Week13/Problem04.java
@@ -8,6 +8,77 @@ package com.mycompany.week13;
  *
  * @author User
  */
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Problem04 {
-    
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(5000);
+            System.out.println("Server started...");
+            System.out.println("Waiting for client...");
+
+            Socket socket = ss.accept();
+            System.out.println("Client connected!");
+
+            DataInputStream dis =
+                    new DataInputStream(socket.getInputStream());
+
+            DataOutputStream dos =
+                    new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                String input = dis.readUTF();
+
+                if (input.equalsIgnoreCase("bye")) {
+                    System.out.println("Client disconnected.");
+                    break;
+                }
+
+                StringTokenizer st = new StringTokenizer(input);
+
+                int num1 = Integer.parseInt(st.nextToken());
+                String operator = st.nextToken();
+                int num2 = Integer.parseInt(st.nextToken());
+
+                int result = 0;
+
+                switch (operator) {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+
+                    case "-":
+                        result = num1 - num2;
+                        break;
+
+                    case "*":
+                        result = num1 * num2;
+                        break;
+
+                    case "/":
+                        if (num2 == 0) {
+                            dos.writeUTF("Cannot divide by zero");
+                            continue;
+                        }
+                        result = num1 / num2;
+                        break;
+
+                    default:
+                        dos.writeUTF("Invalid operator");
+                        continue;
+                }
+
+                dos.writeUTF(String.valueOf(result));
+            }
+
+            socket.close();
+            ss.close();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
